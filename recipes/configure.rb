ef8db60f059e0ba09821b9f8f742e40acd436282
Chris Moultrie <cmoultrie@pindropsecurity.com>
2015-07-22 15:32:45 -0400

    manual hoisting

--- recipes/configure.rb
+++ recipes/configure.rb
@@ -2,14 +2,44 @@
 # Cookbook Name:: apache_kafka
 # Recipe:: configure
 #
-kafka_brokerid = nil
 
-node["apache_kafka"]["servers"].each do |server|
-  if does_server_match_node? server
-    kafka_brokerid = id.to_s
+def does_server_match_node? server
+  # We check that the server value is either the nodes fqdn, hostname or ipaddress.
+  identities = [node["fqdn"], node["hostname"]]
+
+  node["network"]["interfaces"].each_value do |interface|
+    interface["addresses"].each_key do |address|
+        identities << address
+    end
   end
 
-  id = id + 1
+  # We also include ec2 identities as well
+  identities << node["machinename"] if node.attribute?("machinename")
+  identities << node["ec2"]["public_hostname"] if node.attribute?("ec2") && node["ec2"].attribute?("public_hostname")
+  identities << node["ec2"]["public_ipv4"] if node.attribute?("ec2") && node["ec2"].attribute?("public_ipv4")
+
+  identities.each do |id|
+    # We also check if instead the value is of the form [HOST]:[PORT]:[PORT] which is also
+    # valid in the case of defining quorum and leader election ports
+    if server == id || server.start_with?("#{id}:")
+      return true
+    end
+  end
+
+  # Nothing matched
+  false
+end
+
+kafka_brokerid = nil
+
+if node["apache_kafka"] && node["apache_kafka"]["servers"]
+  node["apache_kafka"]["servers"].each do |server|
+    if does_server_match_node? server
+      kafka_brokerid = id.to_s
+    end
+
+    id = id + 1
+  end
 end
 
 [
@@ -71,31 +101,4 @@ template ::File.join(node["apache_kafka"]["config_dir"],
     :entries => node["apache_kafka"]["conf"]["log4j"]["entries"]
   )
   notifies :restart, "service[kafka]", :delayed
-end
-
-def does_server_match_node? server
-  # We check that the server value is either the nodes fqdn, hostname or ipaddress.
-  identities = [node["fqdn"], node["hostname"]]
-
-  node["network"]["interfaces"].each_value do |interface|
-    interface["addresses"].each_key do |address|
-        identities << address
-    end
-  end
-
-  # We also include ec2 identities as well
-  identities << node["machinename"] if node.attribute?("machinename")
-  identities << node["ec2"]["public_hostname"] if node.attribute?("ec2") && node["ec2"].attribute?("public_hostname")
-  identities << node["ec2"]["public_ipv4"] if node.attribute?("ec2") && node["ec2"].attribute?("public_ipv4")
-
-  identities.each do |id|
-    # We also check if instead the value is of the form [HOST]:[PORT]:[PORT] which is also
-    # valid in the case of defining quorum and leader election ports
-    if server == id || server.start_with?("#{id}:")
-      return true
-    end
-  end
-
-  # Nothing matched
-  false
 end
