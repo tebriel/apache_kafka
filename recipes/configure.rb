fbe46156e3a40108eb772dfba8fdc0fcacad0f20
Chris Moultrie <cmoultrie@pindropsecurity.com>
2015-07-22 15:04:47 -0400

    Allowing listing all servers to determine ID

--- recipes/configure.rb
+++ recipes/configure.rb
@@ -2,6 +2,15 @@
 # Cookbook Name:: apache_kafka
 # Recipe:: configure
 #
+kafka_brokerid = nil
+
+node["apache_kafka"]["servers"].each do |server|
+  if does_server_match_node? server
+    kafka_brokerid = id.to_s
+  end
+
+  id = id + 1
+end
 
 [
   node["apache_kafka"]["config_dir"],
@@ -30,7 +39,8 @@ end
 end
 
 broker_id = node["apache_kafka"]["broker.id"]
-broker_id = 0 if broker_id.nil?
+broker_id = kafka_brokerid if kafka_brokerid
+broker_id = 128 if broker_id.nil?
 
 zookeeper_connect = node["apache_kafka"]["zookeeper.connect"]
 zookeeper_connect = "localhost:2181" if zookeeper_connect.nil?
@@ -61,4 +71,31 @@ template ::File.join(node["apache_kafka"]["config_dir"],
     :entries => node["apache_kafka"]["conf"]["log4j"]["entries"]
   )
   notifies :restart, "service[kafka]", :delayed
+end
+
+def does_server_match_node? server
+  # We check that the server value is either the nodes fqdn, hostname or ipaddress.
+  identities = [node["fqdn"], node["hostname"]]
+
+  node["network"]["interfaces"].each_value do |interface|
+    interface["addresses"].each_key do |address|
+        identities << address
+    end
+  end
+
+  # We also include ec2 identities as well
+  identities << node["machinename"] if node.attribute?("machinename")
+  identities << node["ec2"]["public_hostname"] if node.attribute?("ec2") && node["ec2"].attribute?("public_hostname")
+  identities << node["ec2"]["public_ipv4"] if node.attribute?("ec2") && node["ec2"].attribute?("public_ipv4")
+
+  identities.each do |id|
+    # We also check if instead the value is of the form [HOST]:[PORT]:[PORT] which is also
+    # valid in the case of defining quorum and leader election ports
+    if server == id || server.start_with?("#{id}:")
+      return true
+    end
+  end
+
+  # Nothing matched
+  false
 end
